b1277504bd0d82f043ad399a4c50fa293a4e6a95
Nare Karapetyan <nkarapetyan@gmail.com>
2016-12-14 22:03:37 -0500

    rotation updated

--- trt_all/catkin_ws/src/KarapetyanN/turtle_motions/src/turtle_motions.cpp
+++ trt_all/catkin_ws/src/KarapetyanN/turtle_motions/src/turtle_motions.cpp
@@ -11,6 +11,9 @@
 #include <unistd.h>
 #include <tf/transform_listener.h>
 
+
+#define DEBUG
+
 class TurtleMotions {
 	public:
 		// Construst a new TurtleMotions object and hook up this ROS node
@@ -28,7 +31,8 @@ class TurtleMotions {
 				// (the second argument indicates that if multiple command messages are in
 				// the queue to be sent, only the last command will be sent)
 				// commandPub = nh.advertise<geometry_msgs::Twist>("follower_velocity_smoother/raw_cmd_vel", 1);
-				commandPub = nh.advertise<geometry_msgs::Twist>("mobile_base/commands/velocity", 1);
+				commandPub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/navi", 1);
+				//commandPub = nh.advertise<geometry_msgs::Twist>("mobile_base/commands/velocity", 1);
 
 				// Subscribe to the simulated robot's laser scan topic and tell ROS to call
 				// this->commandCallback() whenever a new message is published on that topic
@@ -53,16 +57,22 @@ class TurtleMotions {
 			double roll, pitch;
 			x = msg->pose.pose.position.x;
 			y = msg->pose.pose.position.y;
+#ifdef DEBUG_1
 			std::cout << "Odom x " << x << " -- y " << y << std::endl;
+#endif
 			// x = -msg->pose.pose.position.y;
 			//y = msg->pose.pose.position.x;
 			heading=tf::getYaw(msg->pose.pose.orientation);
+called = true;
+			std::cout << ros::Time::now() << " Odom x " << x << " -- y " << y  << " -- heading " << heading << std::endl;
 		};
 
 		//FIXME: this could be replaced into separate srtuct
 		void poseCallback_comb(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg) {
 			double roll, pitch;
+#ifdef DEBUG_1
 			std::cout << x_comb << " -- " << y_comb << std::endl;
+#endif
 			x_comb = msg->pose.pose.position.x;
 			y_comb = msg->pose.pose.position.y;
 			// x = -msg->pose.pose.position.y;
@@ -73,52 +83,89 @@ class TurtleMotions {
 		void translate(double distance) {
 			ros::Rate rate(10); // Specify the FSM loop rate in Hz
 
-            double odom_d = 0.0;
-            double odom_comb_d = 0.0;
-            double prev_x = x;
-            double prev_y = y;
+			double odom_d = 0.0;
+			double odom_comb_d = 0.0;
+			double prev_x = x;
+			double prev_y = y;
 
-            double prev_x_comb = x_comb;
-            double prev_y_comb = y_comb;
-            
-            ros::Duration time = ros::Duration(distance / FORWARD_SPEED_MPS) ;
+			double prev_x_comb = x_comb;
+			double prev_y_comb = y_comb;
 
-            while (ros::ok()) { // Keep spinning loop until user presses Ctrl+C
-                if(ros::Time::now() - moveStartTime >= time) {
-                    move(0, 0);
-                    break;
-                }
-                move(FORWARD_SPEED_MPS, 0);
-                ros::spinOnce(); // Need to call this function often to allow ROS to process incoming messages
-                rate.sleep(); // Sleep for the rest of the cycle, to enforce the FSM loop rate
-            }
+			ros::Duration time = ros::Duration(distance / FORWARD_SPEED_MPS) ;
 
-            odom_d = prev_x - x;
-            odom_comb_d = prev_x_comb - x_comb;
-            std::cout << "Printing the Odometry information for translate: \n";
-            std::cout << "commanded distance: " << distance << "\n";
-            std::cout << "odom estimate: " <<  odom_d << "\n"; 
-            std::cout << "odom_comb estimate: " <<  odom_comb_d << "\n"; 
-        }
-		
+			while (ros::ok()) { // Keep spinning loop until user presses Ctrl+C
+				if(ros::Time::now() - moveStartTime >= time) {
+					move(0, 0);
+					break;
+				}
+				move(FORWARD_SPEED_MPS, 0);
+				ros::spinOnce(); // Need to call this function often to allow ROS to process incoming messages
+				rate.sleep(); // Sleep for the rest of the cycle, to enforce the FSM loop rate
+			}
+
+			odom_d = sqrt(pow((prev_x - x), 2) + pow((prev_y - y), 2));
+			odom_comb_d = sqrt(pow((prev_x_comb - x_comb), 2) + pow((prev_y_comb - y_comb), 2));
+			std::cout << "Printing the odometry information for translate: \n";
+			std::cout << "commanded distance: " << distance << "\n";
+			std::cout << "odom estimate: " <<  fabs(odom_d) << "\n"; 
+			std::cout << "odom_comb estimate: " <<  fabs(odom_comb_d) << "\n"; 
+		}
+
 
 		//the angle is in degree not in radian
 		void rotate_rel(double angle) {
-			std::cout << "ROTATE_REL -> " << angle << '\n';
-			ros::Duration time = ros::Duration(abs(((angle * M_PI)/180) / ROTATE_SPEED_RADPS));
+#ifdef DEBUG
+			std::cout << "\n*********roation starts**********\n";
+			std::cout << ros::Time::now() << "ROTATE_REL -> " << angle << '\n';
+#endif
+			ros::Rate rate(10); // Specify the FSM loop rate in Hz
+while(!called) {
+				ros::spinOnce(); // Need to call this function often to allow ROS to process incoming messages
+				rate.sleep(); // Sleep for the rest of the cycle, to enforce the FSM loop rate
+} 
+			double odom_angle =0.0;
+			double odom_comb_angle =0.0;
+			double prev_h = heading;
+			double prev_h_comb = heading_comb;
+			std::cout << ros::Time::now() << "ROTATE_REL -> " << angle << '\n';
+
+			ros::Duration time = ros::Duration(fabs(((angle * M_PI)/180) / ROTATE_SPEED_RADPS));
 			double dir = (angle > 0) ? 1.0 : -1.0;
-			while(ros::Time::now() - rotateStartTime < time) {
+			while (ros::ok()) { // Keep spinning loop until user presses Ctrl+C
+				if(ros::Time::now() - rotateStartTime >= time) {
+					move(0, 0);
+					break;
+				}
 				move(0, dir*ROTATE_SPEED_RADPS);
+
+
+				ros::spinOnce(); // Need to call this function often to allow ROS to process incoming messages
+				rate.sleep(); // Sleep for the rest of the cycle, to enforce the FSM loop rate
 			}
-			move(0, 0);
+#ifdef DEBUG
+			std::cout << (heading*180)/M_PI <<std::endl;
+			std::cout << (prev_h*180)/M_PI <<std::endl;
+			std::cout << "\n*********roation ends**********\n";
+#endif
+
+			odom_angle = prev_h - heading;
+			odom_comb_angle = prev_h_comb - heading_comb;
+			std::cout << "Printing the odometry information for relative rotation: \n";
+			std::cout << "commanded distance: " << angle << "\n";
+			std::cout << "odom estimate: " <<  fabs((odom_angle * 180)/M_PI) << "\n"; 
+			std::cout << "odom_comb estimate: " <<  fabs((odom_comb_angle*180)/M_PI) << "\n"; 
 		}
-		
+
 		void rotate_abs(double angle) {
+#ifdef DEBUG
 			std::cout << "ROTATE_ABS -> " << angle << '\n';
+#endif
 			angle = heading - angle;
 			angle = atan2(sin(angle), cos(angle));// to get an angle from [-pi/2, pi/2] angles
 			rotate_rel((angle*180)/M_PI);
+#ifdef DEBUG
 			std::cout << "ROTATE_ABS normailized angle rotated -> " << angle << '\n';
+#endif
 		}
 
 		void make_square(double side) {
@@ -157,7 +204,9 @@ class TurtleMotions {
 
                 if(m_cmd == ROTATE_REL) {
                     rotateStartTime = ros::Time::now();
+#ifdef DEBUG
                     std::cout << "vbbbbbbbbbbbbbbbbbbbb=> " << m_val << std::endl;
+#endif
                     rotate_rel(m_val);
                 }
 		}
@@ -200,6 +249,8 @@ class TurtleMotions {
 		ros::Time moveStartTime; // Start time of the rotation
 		ros::Duration rotateDuration; // Duration of the rotation
 
+bool called  = false;
+
 };
 
 int main(int argc, char **argv) {
@@ -215,7 +266,9 @@ int main(int argc, char **argv) {
 		try {
 			cmd = boost::lexical_cast<int>(argv[1]);
 			val = boost::lexical_cast<double>(argv[2]);
+#ifdef DEBUG
 			std::cout << "val======= " << val << std::endl;
+#endif
 
 			if (cmd < 0) { printUsage = true; }
 			//else if (val <= 0) { printUsage = true; }
